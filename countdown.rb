478d12698ce3a7984b3c690e77e1d113e0db6202
Emily Bolten <emilybolten@gmail.com>
2019-05-01 20:34:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -2,17 +2,29 @@
 def countdown(num)
   count = num
   while count > 0
+<<<<<<< HEAD
     puts "#{count} SECOND(S)!\n"
+=======
+    puts "#{num} SECOND(S)!\n"
+    sleep(1)
+>>>>>>> d13f2e7b4a504681093a29bcdcb787ebb56ba079
     count -= 1
   end
   return "HAPPY NEW YEAR!"
 end
+<<<<<<< HEAD
 
+=======
+>>>>>>> d13f2e7b4a504681093a29bcdcb787ebb56ba079
 
 def countdown_with_sleep(num)
   count = num
   while count > 0
+<<<<<<< HEAD
     puts "#{count} SECOND(S)!\n"
+=======
+    puts "#{num} SECOND(S)!\n"
+>>>>>>> d13f2e7b4a504681093a29bcdcb787ebb56ba079
     sleep(1)
     count -= 1
   end
